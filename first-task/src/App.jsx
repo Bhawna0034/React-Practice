d9244469868c8e95e876fbdf46143a17f6a5bdad
Bhawna0034 <bhawnaasharma2002@gmail.com>
2024-12-17 12:47:57 +0530

    React Conditional Rendering

---
+++ first-task/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// export default function App(){
+//   return(
+//     <>
+//     <h1>Welcome to my app</h1>
+//     <MyButton />
+//     </>
+//   )
+// }
+
+// const MyButton = () => {
+//   return(
+//     <button>Click Me!</button>
+//   )
+// }
+export default function App() {
+  return (
+  <> 
+  <NetflixSeries/>
+  <NetflixSeries/>
+  </>
+  )
+ 
+}
+
+const NetflixSeries = () => {
+  const name = "STRANGER THINGS";
+  const rating = 3.568;
+  const summary = "In a small town where everyone knows everyone, a peculiar incident starts a chain of events that leads to disappereance of child - which begins to tear at the fabric of an otherwise peaceful community...";
+
+  const returnGenre = () => {
+    const genre = "RomCom";
+    return genre;
+  }
+
+  const age = 22;
+  // let canWatch = "NOT AVAILABLE";
+  // if(age>=18){
+  //   canWatch = "WATCH NOW";}
+
+  function canWatch(){
+    if(age>=18) return "WATCH NOW";
+    else return "NOT AVAILABLE";
+  }
+
+  return (
+    <> 
+    <div>
+      <img src='StrangerThings.jpg' alt='StrangerThings' />
+    </div>
+    <h2>Name: {name}</h2>
+    <h3>Ratings: {rating}</h3>
+    <p>Summary: {summary}</p>
+    <p>Genre: {returnGenre()}</p>
+    {/* <button>{age>=18 ? "WATCH NOW" : "NOT AVAILABLE" }</button> */}
+    {/* <button>{canWatch}</button> */}
+    <button>{canWatch()}</button>
+    </>
+    )
+}
+
+
