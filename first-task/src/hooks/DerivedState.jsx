2b7aa4c79c7b3bc1542c7f4126b4a17c593bfc99
Bhawna0034 <bhawnaasharma2002@gmail.com>
2024-12-23 13:24:28 +0530

    Derived State

--- first-task/src/hooks/DerivedState.jsx
+++ first-task/src/hooks/DerivedState.jsx
@@ -8,16 +8,25 @@ const DerivedState = () => {
         {name: "Charlie", age: 35},
         {name: "Angles", age: 45}
     ]);
+
+    // Derived State: count of Users
+    const usersCount = users.length;
+
+    // Derived State: average age of users
+    const averageAge = users.reduce((accum, currentElement) => accum + currentElement.age, 0)/usersCount;
     return(
         <>
         <h2 className="text-2xl font-semibold">User's list</h2>
         <ul>
             {users.map((currentElement, index) => {
                 return(
-                    <li key={index}>{currentElement.name} - {currentElement.age} years old</li>
+                    <li key={index} className="text-xl">{currentElement.name} - {currentElement.age} years old</li>
                 )
             })}
-            </ul></>
+        </ul>
+        <p className="text-xl mt-2">Total Users: {usersCount}</p>
+        <p className="text-xl mt-2">Average Age: {averageAge}</p>
+    </> 
     )
 }
 export default DerivedState;
